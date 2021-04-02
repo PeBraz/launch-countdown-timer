3d251fb345071f92b56231507527b2b8b769100e
Pedro Braz <PeBraz@users.noreply.github.com>
2021-04-02 23:34:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Frontend Mentor - Launch Countdown timer
+This is my solution to this [this challenge](https://www.frontendmentor.io/challenges/launch-countdown-timer-N0XkGfyz-).
+
+Built only with HTML, CSS & Javascript. 
+
+There are 2 fixed layouts for the design:
+ - Destktop @1440px
+ - Mobile @375px
+ 
+## Screenshot
+
+![Desktop Solution](./screenshot.png)
+
+## Improvements
+
+- The animation isn't always being trigered and the styling could be improved to make it more obvious
+- Could add a linear gradient to make it match the design better
---
+++ script.js
@@ -0,0 +1,108 @@
+$ = document.querySelectorAll.bind(document); // Hehe :) poor man's JQuery
+
+const render = (selector, dom) =>
+  $(selector).forEach((ele) => {
+    ele.innerHTML = dom;
+  });
+
+const renderCard = (cardClassName, { time, prevTime }) => {
+  if (!/\.timer__(day|hour|minute|second)/.test(cardClassName)) {
+    throw new Error(`Invalid card class: ${cardClassName}`);
+  }
+
+  const clock = `
+    <div class="timer__card-top">
+      <div class="timer__card-top-digits">
+        ${String(prevTime || time).padStart(2, 0)}
+      </div>
+    </div>
+    <div class="timer__card-bottom">
+      <div class="timer__card-bottom-digits">
+        ${String(prevTime || time).padStart(2, 0)}
+      </div>
+    </div>
+  `;
+
+  if (!prevTime) {
+    render(cardClassName, clock);
+    return;
+  }
+
+  render(
+    cardClassName,
+    `
+    <div class="timer__card-top">
+      <div class="timer__card-top-digits">
+        ${String(time).padStart(2, 0)}
+      </div>
+    </div>
+    ${clock}
+    <div class="timer__card-bottom timer__card-bottom--animated">
+      <div class="timer__card-bottom-digits">
+        ${String(time).padStart(2, 0)}
+      </div>
+    </div>
+  `
+  );
+
+  setTimeout(() => {
+    $(`${cardClassName} .timer__card-top`)[1].classList.add(
+      "timer__card-top--animated"
+    );
+    $(`${cardClassName} .timer__card-bottom`)[1].classList.remove(
+      "timer__card-bottom--animated"
+    );
+  }, 10);
+};
+
+const renderClock = (prevTime, { days, hours, minutes, seconds }) => {
+  if (prevTime.days !== days) {
+    renderCard(".timer__day", { prevTime: prevTime.days, time: days });
+  }
+
+  if (prevTime.hours !== hours) {
+    renderCard(".timer__hour", { prevTime: prevTime.hours, time: hours });
+  }
+
+  if (prevTime.minutes !== minutes) {
+    renderCard(".timer__minute", { prevTime: prevTime.minutes, time: minutes });
+  }
+
+  if (prevTime.seconds !== seconds) {
+    renderCard(".timer__second", { prevTime: prevTime.seconds, time: seconds });
+  }
+};
+
+function countdown(days) {
+  let currentTime = {
+    days: days,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  };
+  renderClock({}, currentTime);
+
+  let seconds = days * 24 * 60 * 60;
+  const interval = setInterval(() => {
+    if (seconds <= 0) {
+      clearInterval(interval);
+      return;
+    }
+    seconds -= 1;
+
+    const minutes = seconds / 60;
+    const hours = minutes / 60;
+    const days = hours / 24;
+    const nextTime = {
+      days: Math.floor(days % 99),
+      hours: Math.floor(hours % 24),
+      minutes: Math.floor(minutes % 60),
+      seconds: Math.floor(seconds % 60),
+    };
+
+    renderClock(currentTime, nextTime);
+    currentTime = nextTime;
+  }, 1000);
+}
+
+countdown(14);
